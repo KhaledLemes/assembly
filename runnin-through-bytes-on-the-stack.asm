134e4d36c2ce02225bdee3caf4fb4c360a9b92a3
KhaledLemes <khaled.borges@outlook.com>
2026-07-07 20:40:47 -0300

    learned a bit more about data on memory -learned about stack pointer -created a code to declare 3 longs and run through each byte of each one with jl and cmp

---
+++ runnin-through-bytes-on-the-stack.asm
@@ -0,0 +1,21 @@
+.intel_syntax noprefix
+.global _start
+.text
+
+_start:
+mov r8, 0
+lea rsp, [var]
+lea rcx, [var]
+
+mov al, [rcx]
+
+inc rcx
+inc r8
+cmp r8, 24
+jl 0x00401017
+
+
+.data
+var: .quad 0x93550000375a02ff, 0x50c28ba4cdce6be1 ,0x1a1a1a081cc7258d
+
+
